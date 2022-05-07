c1619fd904a786e2dc703f09ee0cd8da834af60f
Aditi Singh <aditishasha2719@gmail.com>
2022-05-07 18:24:01 +0530

    Created Header component

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header__left">
+        <div className="logo">
+            <h1>NFT Ticketing</h1>
+            <h3>Marketplace</h3>
+        </div>
+        <div className="header__search">
+            <SearchIcon/>
+            <input placeholder='Search items, collections and accounts' type="text"/>
+         </div>
+        <div className="header__right">
+        <ul>
+            <li><a class="navitem" href="">Discover</a></li>
+            <li><a class="navitem" href="">Create Event</a></li>
+            <li><a class="navitem" href="">Connect Wallet</a></li>
+       </ul>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Header
